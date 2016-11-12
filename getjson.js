4af9aaaae172194bbeccab021bec4449d0bedefe
Mikhail Martyushov <martyushovmv@gmail.com>
2016-11-12 21:32:06 +0300

    script

--- getjson.js
+++ getjson.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function() {
 
-$.getJSON('users.json', function(data) {
+$.getJSON('https://github.com/MartyushovMV/MartyushovMV.github.io/blob/master/users.json', function(data) {
       for(var i=0;i<data.UserStats.length;i++){
                  $('#users').append('<tr><td>' + data.UserStats[i].id + '</td><td>' + data.UserStats[i].userURL + 
                 '</td><tr>');
