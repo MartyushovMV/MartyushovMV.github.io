cbc0f3ebe92613762482ad7919b0e572a48f119b
Mikhail Martyushov <martyushovmv@gmail.com>
2016-11-12 21:27:46 +0300

    ajax - JSON

---
+++ getjson.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function() {
+
+$.getJSON('users.json', function(data) {
+      for(var i=0;i<data.UserStats.length;i++){
+                 $('#users').append('<tr><td>' + data.UserStats[i].id + '</td><td>' + data.UserStats[i].userURL + 
+                '</td><tr>');
+            }
+    });
+});
