b406d8da0d077f76d1c477eb43a9198e621854cd
Mikhail Martyushov <martyushovmv@gmail.com>
2016-11-13 18:11:14 +0300

    Ajax - json

--- getjson.js
+++ getjson.js
@@ -1,9 +1,12 @@
-﻿$(document).ready(function() {
-
-$.getJSON('https://github.com/MartyushovMV/MartyushovMV.github.io/blob/master/users.json', function(data) {
-      for(var i=0;i<data.UserStats.length;i++){
-                 $('#users').append('<tr><td>' + data.UserStats[i].id + '</td><td>' + data.UserStats[i].userURL + 
-                '</td><tr>');
-            }
-    });
+﻿$.ajax({
+  url: 'users.json',
+  dataType: 'json',
+  type: 'get',
+  cache: 'false',
+  success: function(data) {
+              $(data.UserStats).each(function(index, value) {
+        $('#wrap').append(
+'<div class="flex-item">' + '<table cellpadding="1" cellspacing="1" >' + '<tr><td colspan="1" rowspan="5"><img src="images/' + value.userName + '.jpg"></td>' + '<td colspan="2" rowspan="1"><a href="https://zap.ru/users/' + value.userName + '">' + value.userName + '</a></td></tr>' + '<tr><td>Группа:</td>' + '<td>' + value.userGroup + '</td></tr>' +'<tr><td>Комментариев:</td>' + '<td>' + value.comments + '</td></tr>' + '<tr><td>Друзей:</td>' + '<td>' + value.friends + '</td></tr>' + '<tr><td>Регистрация:</td>' + '<td>' + value.registrationDate + '</td></tr>' + '<tr><td>' + value.rank + '</td>' + '<td> Активность:</td>' + '<td>' + value.timeLastActivities + '</td></tr>' + '</table>' + '</div>');
+     });
+   }
 });
