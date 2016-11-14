9c610b99d26a1451c906320cc2345edaf9822d86
Mikhail Martyushov <martyushovmv@gmail.com>
2016-11-14 12:41:29 +0300

    Ajax - json

--- Ajax_json/j/getjson.js
+++ Ajax_json/j/getjson.js
@@ -6,7 +6,7 @@
   success: function(data) {
               $(data.UserStats).each(function(index, value) {
         $('#wrap').append(
-'<div class="flex-item">' + '<table cellpadding="1" cellspacing="1" >' + '<tr><td colspan="1" rowspan="5"><img src="images/' + value.userName + '.jpg"></td>' + '<td colspan="2" rowspan="1"><a href="https://zap.ru/users/' + value.userName + '">' + value.userName + '</a></td></tr>' + '<tr><td>Группа:</td>' + '<td>' + value.userGroup + '</td></tr>' +'<tr><td>Комментариев:</td>' + '<td>' + value.comments + '</td></tr>' + '<tr><td>Друзей:</td>' + '<td>' + value.friends + '</td></tr>' + '<tr><td>Регистрация:</td>' + '<td>' + value.registrationDate + '</td></tr>' + '<tr><td>' + value.rank + '</td>' + '<td> Активность:</td>' + '<td>' + value.timeLastActivities + '</td></tr>' + '</table>' + '</div>');
+'<div class="flex-item">' + '<table cellpadding="1" cellspacing="1" >' + '<tr><td colspan="1" rowspan="5"><img src="images/' + value.userName + '.jpg"></td>' + '<td colspan="2" rowspan="1"><a href="https://zap.ru/users/' + value.userName + '">' + value.userName + '</a></td></tr>' + '<tr><td>Группа:</td>' + '<td>' + value.userGroup + '</td></tr>' +'<tr><td>Комментариев:</td>' + '<td>' + value.comments + '</td></tr>' + '<tr><td>Друзей:</td>' + '<td>' + value.friends + '</td></tr>' + '<tr><td>Регистрация:</td>' + '<td>' + value.registrationDate + '</td></tr>' + '<tr><td align="center">' + value.rank + '</td>' + '<td> Активность:</td>' + '<td>' + value.timeLastActivities + '</td></tr>' + '</table>' + '</div>');
      });
    }
 });
