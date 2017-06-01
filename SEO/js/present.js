6ce2fe3b1f1bbc4ea8f1996d93586ccc86bbf5c9
Mikhail Martyushov <martyushovmv@gmail.com>
2017-06-01 10:48:57 +0300

    SEO website

---
+++ SEO/js/present.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    PopUpHide();
+	PopUpHidez();
+});
+function PopUpShow(){
+    $("#popup1").show();
+	$("#verh").hide();
+}
+function PopUpHide(){
+    $("#popup1").hide();
+	$("#verh").show();
+}
+function PopUpShowz(){
+    $("#popup2").show();
+	$("#verh").hide();
+}
+function PopUpHidez(){
+    $("#popup2").hide();
+	$("#verh").show();
+}
