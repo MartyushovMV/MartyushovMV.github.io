7d9d9a6d5830d1c198480a5cfdf595c6c6cb5204
Mikhail Martyushov <martyushovmv@gmail.com>
2017-06-04 01:57:50 +0300

    SEO website

---
+++ SEO/js/popup.js
@@ -0,0 +1,10 @@
+    //Функция отображения PopUp
+    function PopUpShow(){
+        $("#popup1").show();
+		$("#verh").hide();
+    }
+    //Функция скрытия PopUp
+    function PopUpHide(){
+        $("#popup1").hide();
+		$("#verh").show();
+    }
