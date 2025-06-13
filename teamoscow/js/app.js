fde5e77bed5621e38481b35d7a2916398d4725fe
Mikhail <martyushovmv@gmail.com>
2025-06-14 02:23:42 +0300

    Update app.js

--- teamoscow/js/app.js
+++ teamoscow/js/app.js
@@ -5,6 +5,10 @@ async function initMap() {
 
     const {YMap, YMapDefaultSchemeLayer, YMapControls} = ymaps3;
 
+    ymaps3.import.registerCdn('https://cdn.jsdelivr.net/npm/{package}', [
+      '@yandex/ymaps3-default-ui-theme@0.0.19'
+    ]);
+
     const {YMapZoomControl} = await ymaps3.import('@yandex/ymaps3-default-ui-theme');
 
     const map = new YMap(
