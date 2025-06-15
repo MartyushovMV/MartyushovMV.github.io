697578755b48c6af27a6f3eab51543df9e403f6f
Mikhail <martyushovmv@gmail.com>
2025-06-15 15:49:02 +0300

    Update ymaps.js

--- teamoscow/js/ymaps.js
+++ teamoscow/js/ymaps.js
@@ -3,13 +3,14 @@ initMap();
 async function initMap() {
     await ymaps3.ready;
 
-    const {YMap, YMapDefaultSchemeLayer, YMapControls} = ymaps3;
+    const {YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer, YMapControls, YMapControl, YMapComplexEntity} = ymaps3;
 
     ymaps3.import.registerCdn('https://cdn.jsdelivr.net/npm/{package}', [
       '@yandex/ymaps3-default-ui-theme@0.0.19'
     ]);
 
     const {YMapZoomControl} = await ymaps3.import('@yandex/ymaps3-default-ui-theme');
+    const {YMapGeolocationControl} = await ymaps3.import('@yandex/ymaps3-default-ui-theme');
 
     const map = new YMap(
       document.getElementById('map'),
@@ -19,11 +20,58 @@ async function initMap() {
           zoom: 10
         }
       },
-      [new YMapDefaultSchemeLayer({})]
+      [
+        new YMapDefaultSchemeLayer({}),
+        new YMapDefaultFeaturesLayer({})
+      ]
     );
 
-    map.addChild(
-      new YMapControls({position: 'right'}).addChild(new YMapZoomControl({}))
-    );
+    const controls = new YMapControls({position: 'right'});
+    controls.addChild(new YMapZoomControl({}));
+    controls.addChild(new YMapGeolocationControl());
+    map.addChild(controls);
+
+      function fullScreenBtnHandler() {
+          if (document.fullscreenElement) {
+              document.exitFullscreen();
+          } else {
+              map.container.requestFullscreen();
+          }
+      }
+
+          class FullscreenButton extends YMapComplexEntity<{}> {
+              private _element: HTMLButtonElement;
+
+              private _detachDom: () => void;
+
+              _createElement() {
+                  const fullScreenButtonElement = document.createElement('button');
+                  fullScreenButtonElement.type = 'button';
+                  fullScreenButtonElement.onclick = fullScreenBtnHandler;
+                  fullScreenButtonElement.classList.add('button', 'fullscreen');
+
+                  document.addEventListener('fullscreenchange', function () {
+                      fullScreenButtonElement.classList.toggle('exit-fullscreen');
+                  });
+
+                  return fullScreenButtonElement;
+              }
+
+              _onAttach() {
+                  this._element = this._createElement();
+                  this._detachDom = ymaps3.useDomContext(this, this._element, this._element);
+              }
+
+              _onDetach() {
+                  this._detachDom();
+                  this._detachDom = null;
+                  this._element = null;
+              }
+          }
+
+          const fullScreenBtn = new YMapControl();
+          fullScreenBtn.addChild(new FullscreenButton({}));
+
+          controls.addChild(fullScreenBtn);
 
 }
