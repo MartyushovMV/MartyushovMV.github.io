eba149c66429a04e9b0061aa03d6e1e371331217
Mikhail <martyushovmv@gmail.com>
2025-06-18 02:14:33 +0300

    Update ymaps.js

--- teamoscow/js/ymaps.js
+++ teamoscow/js/ymaps.js
@@ -124,7 +124,7 @@ async function initMap() {
       popup: {content: PopupWithImage, position: 'top'}
     });
 
-    const titlez = popupWithImage.properties.get('title');
+    const titlez = popupWithImage.props.get('title');
 
     popup2 = new YMapDefaultMarker({
       iconName: 'cafe',
