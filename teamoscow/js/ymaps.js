e7e4213b8dc65b64d3d8561922b80028bb268775
Mikhail <martyushovmv@gmail.com>
2025-06-22 20:59:56 +0300

    Update ymaps.js

--- teamoscow/js/ymaps.js
+++ teamoscow/js/ymaps.js
@@ -198,7 +198,7 @@ async function initMap() {
           let bounds;
           map.update({
             location: {
-              center: [37.610524, 55.782051],
+              center: [37.610524, 55.802051],
               zoom: 10,
               bounds,
               duration: 400
